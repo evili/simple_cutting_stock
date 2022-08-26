832f0bb7cfedd563f5ae8eeda97696bee3fadb75
Evili del Rio <evili@iiia.csic.es>
2022-08-26 16:47:23 +0200

    Pretty print results
    Signed-off-by: Evili del Rio <evili@iiia.csic.es>

--- stock_cutting.py
+++ stock_cutting.py
@@ -27,7 +27,7 @@ orders = sorted(get_data('order_cuts.txt'), key=lambda cut: cut[LENGTH], reverse
 stock_rolls = length_list(stocks)
 order_cuts = length_list(orders)
 
-print(f"Stock Rolls: {stock_rolls}")
+# print(f"Stock Rolls: {stock_rolls}")
 #
 # Check Stock Length
 #
@@ -40,9 +40,9 @@ assert stock_total >= order_total, f"Not enough stock roll ({stock_total}) for o
 roll_cuts=[]
 for roll in stock_rolls:
     roll_cuts.append([])
-print("THE NAIVE CUTTING PROBLEM STRATEGY:")
-print(f"Cutting {order_cuts} order from {stock_rolls} stock rolls")
-print(f"Order length: {order_total}, stock length: {stock_total}")
+print("\n\nTHE NAIVE CUTTING PROBLEM STRATEGY:")
+#print(f"\tCutting {order_cuts} order from {stock_rolls} stock rolls")
+print(f"\tOrder length: {order_total}, stock length: {stock_total}\n\n")
 #
 # Take the maximal cut
 #
@@ -54,15 +54,15 @@ cutted_orders = []
 
 while pivot_roll < len(stock_rolls):
     largest_cut = np.max(length_list(uncut_orders))
-    print(f"Largest cut: {largest_cut}")
+    #print(f"Largest cut: {largest_cut}")
     next_cut = length_list(uncut_orders).index(largest_cut)
     if (stock_rolls[pivot_roll] - sum(length_list(cutted_orders))) >= largest_cut:
         # add to cutted_orders
-        print(f"Cutting {largest_cut} from {pivot_roll}")
+        #print(f"Cutting {largest_cut} from {pivot_roll}")
         cutted_orders.append(uncut_orders.pop(next_cut))
     else:
         # add to not_fitted_orders
-        print(f"Cut {largest_cut} does not fit on {pivot_roll}")
+        #print(f"Cut {largest_cut} does not fit on {pivot_roll}")
         not_fitted_orders.append(uncut_orders.pop(next_cut))
 
     if (len(uncut_orders) == 0):
@@ -78,12 +78,14 @@ while pivot_roll < len(stock_rolls):
 #
 # We have tried to cut all rolls so there should be no uncut_orders
 #
-print("==> RESULTS:")
+print("\n\n===> RESULTS:")
 wasted_length = []
 for roll_index, roll_length in enumerate(stock_rolls):
     wasted_length.append(roll_length-sum(length_list(roll_cuts[roll_index])))
-    print(f"Roll {roll_index} ({roll_length}): {roll_cuts[roll_index]}; W: {wasted_length[roll_index]}")
-print(f"Total WASTE: {sum(wasted_length)}")
+    print(f"\nRoll {roll_index}: {stocks[roll_index][LABEL]} of  {roll_length}:\n\tWasted:\t\t{wasted_length[roll_index]:5.1f}")
+    for cut in roll_cuts[roll_index]:
+        print(f"\t{cut[LABEL]:8s} of\t{cut[LENGTH]:5.1f}")
+print(f"\nTotal WASTE: {sum(wasted_length)}")
 if len(uncut_orders) > 0:
     print(f"Uncut orders: {uncut_orders}")
     print(f"Sorry there are still som cuts to be done: {uncut_orders}")
