56801ff4c9d839ce2152774876b28198c2cef6dd
Evili del Rio <evili@iiia.csic.es>
2022-08-26 16:07:07 +0200

    Move whole (label, length) records.
    Signed-off-by: Evili del Rio <evili@iiia.csic.es>

--- stock_cutting.py
+++ stock_cutting.py
@@ -4,19 +4,29 @@
 #
 import numpy as np
 from read_lengths import get_data
+LABEL = 0
+LENGTH = 1
+
+def length_list(label_length):
+    return list(x[LENGTH] for x in label_length)
+
+def label_list(label_lengt):
+    return list(x[LABEL] for x in label_length)
+
 # order_cuts.txt  stock_rolls.txt
-stocks = sorted(get_data('stock_rolls.txt'), key=lambda roll: roll[1], reverse=True)
+stocks = sorted(get_data('stock_rolls.txt'), key=lambda roll: roll[LENGTH], reverse=True)
 # stock_labels, stock_rolls = get_data('stock_rolls.txt')
 # [1000, 1000, 650]
-orders = sorted(get_data('order_cuts.txt'), key=lambda cut: cut[1], reverse=True)
+orders = sorted(get_data('order_cuts.txt'), key=lambda cut: cut[LENGTH], reverse=True)
 # order_labels, order_cuts =  get_data('order_cuts.txt')
 # [450, 656, 400, 234, 234, 444,95,12]
 #
 # Sorted version
 #
 
-stock_rolls = list(x[1] for x in stocks)
-order_cuts = list(x[1] for x in orders)
+stock_rolls = length_list(stocks)
+order_cuts = length_list(orders)
+
 print(f"Stock Rolls: {stock_rolls}")
 #
 # Check Stock Length
@@ -37,14 +47,16 @@ print(f"Order length: {order_total}, stock length: {stock_total}")
 # Take the maximal cut
 #
 pivot_roll = 0
-uncut_orders = order_cuts.copy()
+uncut_orders = orders
+# _cuts.copy()
 not_fitted_orders = []
 cutted_orders = []
 
 while pivot_roll < len(stock_rolls):
-    largest_cut = np.max(uncut_orders)
-    next_cut = uncut_orders.index(largest_cut)
-    if (stock_rolls[pivot_roll] - sum(cutted_orders)) >= largest_cut:
+    largest_cut = np.max(length_list(uncut_orders))
+    print(f"Largest cut: {largest_cut}")
+    next_cut = length_list(uncut_orders).index(largest_cut)
+    if (stock_rolls[pivot_roll] - sum(length_list(cutted_orders))) >= largest_cut:
         # add to cutted_orders
         print(f"Cutting {largest_cut} from {pivot_roll}")
         cutted_orders.append(uncut_orders.pop(next_cut))
@@ -69,7 +81,7 @@ while pivot_roll < len(stock_rolls):
 print("==> RESULTS:")
 wasted_length = []
 for roll_index, roll_length in enumerate(stock_rolls):
-    wasted_length.append(roll_length-sum(roll_cuts[roll_index]))
+    wasted_length.append(roll_length-sum(length_list(roll_cuts[roll_index])))
     print(f"Roll {roll_index} ({roll_length}): {roll_cuts[roll_index]}; W: {wasted_length[roll_index]}")
 print(f"Total WASTE: {sum(wasted_length)}")
 if len(uncut_orders) > 0:
@@ -77,11 +89,3 @@ if len(uncut_orders) > 0:
     print(f"Sorry there are still som cuts to be done: {uncut_orders}")
 else:
     print(f"Job DONE!")
-
-#
-# Refine the cut
-#
-if max(wasted_length) >= min(order_cuts):
-    print(f"Cut could be refined: {max(wasted_length)} {min(order_cuts)}")
-else:
-    print("Cut is OPTIMAL")
